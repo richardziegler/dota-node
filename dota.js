b62245034cbf7cad34a2c7d20ab63f2ced18b2b5
Ziegler <zieglerr@stifel.com>
2019-09-04 15:33:15 -0500

    Set up Axios, getting steam ID - RZ

---
+++ dota.js
@@ -0,0 +1,31 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const axios = require('axios');
+
+const steamAPIKey = 'xxxx';
+// const openDotaApiKey = 'xxxx';
+let steamUsername = '';
+
+console.log('   ___  ____  _________     ___  ___  ____  __________   ____\n' +
+'  / _ \\/ __ \\/_  __/ _ |   / _ \\/ _ \\/ __ \\/ __/  _/ /  / __/\n' +
+' / // / /_/ / / / / __ |  / ___/ , _/ /_/ / _/_/ // /__/ _/  \n' +
+'/____/\\____/ /_/ /_/ |_| /_/  /_/|_|\\____/_/ /___/____/___/  \n' +
+'                                                             ');
+
+readline.question(`What is your Steam username?`, (name) => {
+    steamUsername = name
+});
+
+axios.get(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamAPIKey}&vanityurl=${steamUsername}`)
+.then(function (response) {
+    console.log(response)
+})
+.catch(function (error) {
+    console.log('error: ' + error)
+})
+.finally(function() {
+
+})
