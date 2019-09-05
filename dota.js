7d0978c28f010aa6df8fba4b61d9573caa3889fd
Richard Ziegler <rzieglerdevelopment@gmail.com>
2019-09-04 22:07:41 -0500

    Tried to fix axios call before readline is done

--- dota.js
+++ dota.js
@@ -1,31 +1,34 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-const axios = require('axios');
+const axios = require("axios");
 
-const steamAPIKey = 'xxxx';
+const steamAPIKey = "xxxx";
 // const openDotaApiKey = 'xxxx';
-let steamUsername = '';
+let steamUsername = "";
 
-console.log('   ___  ____  _________     ___  ___  ____  __________   ____\n' +
-'  / _ \\/ __ \\/_  __/ _ |   / _ \\/ _ \\/ __ \\/ __/  _/ /  / __/\n' +
-' / // / /_/ / / / / __ |  / ___/ , _/ /_/ / _/_/ // /__/ _/  \n' +
-'/____/\\____/ /_/ /_/ |_| /_/  /_/|_|\\____/_/ /___/____/___/  \n' +
-'                                                             ');
+console.log(
+  "   ___  ____  _________     ___  ___  ____  __________   ____\n" +
+    "  / _ \\/ __ \\/_  __/ _ |   / _ \\/ _ \\/ __ \\/ __/  _/ /  / __/\n" +
+    " / // / /_/ / / / / __ |  / ___/ , _/ /_/ / _/_/ // /__/ _/  \n" +
+    "/____/\\____/ /_/ /_/ |_| /_/  /_/|_|\\____/_/ /___/____/___/  \n" +
+    "                                                             "
+);
 
-readline.question(`What is your Steam username?`, (name) => {
-    steamUsername = name
+readline.question(`What is your Steam username?`, name => {
+  steamUsername = name;
 });
 
-axios.get(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamAPIKey}&vanityurl=${steamUsername}`)
-.then(function (response) {
-    console.log(response)
-})
-.catch(function (error) {
-    console.log('error: ' + error)
-})
-.finally(function() {
-
-})
+let getSteamId = axios
+  .get(
+    `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamAPIKey}&vanityurl=${steamUsername}`
+  )
+  .then(function(response) {
+    console.log(response.data);
+  })
+  .catch(function(error) {
+    console.log("error: " + error);
+  })
+  .finally(function() {});
